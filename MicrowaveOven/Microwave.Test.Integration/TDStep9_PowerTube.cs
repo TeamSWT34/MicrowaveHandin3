e977557407777aa75e2d4569772b94fff65d6f64
TheGuardiaan <casperfevre@gmail.com>
2021-04-26 18:57:58 +0200

    TDStep9_PowerTube testet

--- MicrowaveOven/Microwave.Test.Integration/TDStep9_PowerTube.cs
+++ MicrowaveOven/Microwave.Test.Integration/TDStep9_PowerTube.cs
@@ -1,29 +1,90 @@
-﻿using NUnit.Framework;
+﻿using Microwave.Classes.Boundary;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace Microwave.Test.Integration
 {
     [TestFixture]
     public class TDStep9_PowerTube
     {
-        
+        private Button sut_PowerButton;
+        private Button sut_TimeButton;
+        private Button sut_StartCancelButton;
+        private Door sut_Door;
+
+        private CookController cookController;
+        private Timer timer;
+        private PowerTube powerTube;
+
+
+        private UserInterface userInterface;
+        private Display display;
+        private Light light;
+
+        private IOutput fakeOutput;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            sut_PowerButton = new Button();
+            sut_TimeButton = new Button();
+            sut_StartCancelButton = new Button();
+            sut_Door = new Door();
+
+            fakeOutput = Substitute.For<IOutput>();
+
+            display = new Display(fakeOutput);
+            timer = new Timer();
+            powerTube = new PowerTube(fakeOutput);
+
+
+            cookController = new CookController(timer, display, powerTube);
+            light = new Light(fakeOutput);
+
+            userInterface = new UserInterface(sut_PowerButton, sut_TimeButton,
+                sut_StartCancelButton, sut_Door, display,
+                light, cookController);
+
+            cookController.UI = userInterface;
+        }
+
+        [Test]
+        public void PowerTube_StartStopButton_Start_Output()
+        {
+            int power = 100;
+
+            sut_PowerButton.Press();
+            sut_PowerButton.Press();
+            sut_TimeButton.Press();
+            sut_StartCancelButton.Press();
+            fakeOutput.Received().OutputLine($"PowerTube works with {power}");
+        }
+
+        [Test]
+        public void PowerTube_StartStopButton_Stop_Output()
+        {
+            sut_PowerButton.Press();
+            sut_PowerButton.Press();
+            sut_TimeButton.Press();
+            sut_StartCancelButton.Press();
+            sut_StartCancelButton.Press();
+            fakeOutput.Received().OutputLine($"PowerTube turned off");
+        }
+
+        [Test]
+        public void PowerTube_OpenDoor_Start_Output()
+        {
+
+            sut_PowerButton.Press();
+            sut_TimeButton.Press();
+            sut_StartCancelButton.Press();
+            sut_Door.Open();
+            fakeOutput.Received().OutputLine($"PowerTube turned off");
+
+        }
+
     }
-
-
-    /*
-    [Test]
-    public void Display_StartStopButton_Press_Clear_LogLine_Output()
-    {
-    int power = 100;
-
-    sut_PowerButton.Press();
-    sut_StartCancelButton.Press();
-    fakeOutput.Received().OutputLine($"Display cleared");
-
-    sut_PowerButton.Press();
-    sut_PowerButton.Press();
-    sut_TimeButton.Press();
-    sut_StartCancelButton.Press();
-    fakeOutput.Received().OutputLine($"PowerTube works with {power}");
-    }
-    */
 }
