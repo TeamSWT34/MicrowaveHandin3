05dca14a4de4e06e14bae239c712bc6efc59b2a4
TheGuardiaan <casperfevre@gmail.com>
2021-04-26 18:29:56 +0200

    TDStep7_Display

--- MicrowaveOven/Microwave.Test.Integration/TDStep7_Display.cs
+++ MicrowaveOven/Microwave.Test.Integration/TDStep7_Display.cs
@@ -1,4 +1,5 @@
-﻿using Microwave.Classes.Boundary;
+﻿using System;
+using Microwave.Classes.Boundary;
 using Microwave.Classes.Controllers;
 using Microwave.Classes.Interfaces;
 using NSubstitute;
@@ -34,57 +35,52 @@ namespace Microwave.Test.Integration
             sut_StartCancelButton = new Button();
             sut_Door = new Door();
 
-            light = new Light(fakeOutput);
+            fakeOutput = Substitute.For<IOutput>();
 
             display = new Display(fakeOutput);
-
-            cookController = new CookController(fakeTimer, display, fakePowerTube);
             fakeTimer = Substitute.For<ITimer>();
             fakePowerTube = Substitute.For<IPowerTube>();
+            
+
+            cookController = new CookController(fakeTimer, display, fakePowerTube);
+            light = new Light(fakeOutput);
 
             userInterface = new UserInterface(sut_PowerButton, sut_TimeButton,
                 sut_StartCancelButton, sut_Door, display,
                 light, cookController);
+
+            cookController.UI = userInterface;
         }
 
 
-/*
         [Test]
         public void Display_PowerPress_LogLine_Output()
         {
             int power = 50;
 
             sut_PowerButton.Press();
-
-            fakeOutput.OutputLine($"Display shows: {power} W");
+            fakeOutput.Received().OutputLine($"Display shows: {power} W");
         }
 
         [Test]
         public void Display_TimePress_LogLine_Output()
         {
-            int min = 1;
+            int min = 2;
             int sec = 0;
 
             sut_PowerButton.Press();
             sut_TimeButton.Press();
-
-            fakeOutput.OutputLine($"Display shows: {min:D2}:{sec:D2}");
+            sut_TimeButton.Press();
+            fakeOutput.Received().OutputLine($"Display shows: {min:D2}:{sec:D2}");
         }
 
 
         [Test]
         public void Display_StartStopButton_Press_Clear_LogLine_Output()
         {
-
             sut_PowerButton.Press();
             sut_StartCancelButton.Press();
-            fakeOutput.OutputLine($"Display cleared");
-
-            sut_PowerButton.Press();
-            sut_TimeButton.Press();
-            sut_CookController.StartCooking(50,60);
-            sut_StartCancelButton.Press();
-            fakeOutput.OutputLine($"Display cleared");
+            fakeOutput.Received().OutputLine($"Display cleared");
         }
 
         [Test]
@@ -93,20 +89,20 @@ namespace Microwave.Test.Integration
 
             sut_PowerButton.Press();
             sut_Door.Open();
-            fakeOutput.OutputLine($"Display cleared");
+            fakeOutput.Received().OutputLine($"Display cleared");
             sut_Door.Close();
 
             sut_PowerButton.Press();
             sut_TimeButton.Press();
             sut_Door.Open();
-            fakeOutput.OutputLine($"Display cleared");
+            fakeOutput.Received().OutputLine($"Display cleared");
             sut_Door.Close();
 
             sut_PowerButton.Press();
             sut_TimeButton.Press();
-            sut_CookController.StartCooking(50, 60);
+            sut_StartCancelButton.Press();
             sut_Door.Open();
-            fakeOutput.OutputLine($"Display cleared");
+            fakeOutput.Received().OutputLine($"Display cleared");
         }
 
         [Test]
@@ -118,20 +114,17 @@ namespace Microwave.Test.Integration
 
             sut_PowerButton.Press();
             sut_TimeButton.Press();
-            sut_CookController.StartCooking(50, timeInSec);
+            sut_StartCancelButton.Press();
 
-            fakeOutput.OutputLine($"Display shows: {min:D2}:{sec:D2}");
-
-            min = 0;
-            for (int i = 0; i < timeInSec-1; i++)
-            {
-                fakeOutput.OutputLine($"Display shows: {min:D2}:{sec:D2}");
-                sec--;
-            }
             
+            fakeTimer.TimeRemaining.Returns(59); //TimeRemaining is set to 59 sec.
+            fakeTimer.TimerTick += Raise.EventWith(this, EventArgs.Empty);
+            fakeOutput.Received().OutputLine($"Display shows: 00:59");
+
+
         }
 
-        */
+        
 
     }
 }
--- MicrowaveOven/Microwave.Test.Integration/TDStep9_PowerTube.cs
+++ MicrowaveOven/Microwave.Test.Integration/TDStep9_PowerTube.cs
@@ -7,4 +7,23 @@ namespace Microwave.Test.Integration
     {
         
     }
+
+
+    /*
+    [Test]
+    public void Display_StartStopButton_Press_Clear_LogLine_Output()
+    {
+    int power = 100;
+
+    sut_PowerButton.Press();
+    sut_StartCancelButton.Press();
+    fakeOutput.Received().OutputLine($"Display cleared");
+
+    sut_PowerButton.Press();
+    sut_PowerButton.Press();
+    sut_TimeButton.Press();
+    sut_StartCancelButton.Press();
+    fakeOutput.Received().OutputLine($"PowerTube works with {power}");
+    }
+    */
 }
